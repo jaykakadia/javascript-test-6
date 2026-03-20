31758db5596c56930726250a0940da472ccd7f16
Jay <jaykakadia3@gmail.com>
2026-03-20 18:17:29 +0530

    added change in 2nd program

--- program-2.js
+++ program-2.js
@@ -1,5 +1,6 @@
 // Write a JavaScript a function that makes an HTTP GET request and returns a Promise that resolves with the response data.
 
 function fetchdata(link) {
-  return fetch(link).then((res) => res.json());
+  return fetch(link).then((res) => res.json()).then((data) => console.log(data));
 }
+fetchdata("https://jsonplaceholder.typicode.com/posts/1");
